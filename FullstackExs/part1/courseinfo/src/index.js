9d63efcb4979de01537752afdf041ccbe563824b
Thomync <thomync9876@gmail.com>
2023-08-26 00:52:28 -0600

    Updated code notes and course exercises

--- FullstackExs/part1/courseinfo/src/index.js
+++ FullstackExs/part1/courseinfo/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+//Exercises 1.1 & 1.2
+/*
 const App = () => {
     
   const header = 'Half Stack application development'
@@ -25,6 +27,7 @@ const App = () => {
 }
 
 const Header = (props) => {
+  console.log(props)
 
   return(
     <div>
@@ -36,6 +39,7 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props)
 
     return (
       <>
@@ -47,6 +51,8 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
+  console.log(props)
+
     return(
       <>
         <p>Total exercises: {props.etotal}</p>
@@ -55,6 +61,8 @@ const Total = (props) => {
 }
 
 const Part = (props) => {
+  console.log(props)
+
   return (
     <>
       <li>
@@ -63,4 +71,234 @@ const Part = (props) => {
     </>
   )
 }
+
+//Exercise 1.3 
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header titulo={course}/>
+      <Content 
+        p1={part1.name} e1={part1.exercises}
+        p2={part2.name} e2={part2.exercises}
+        p3={part3.name} e3={part3.exercises}
+      />
+      <Total etotal={part1.exercises + part2.exercises + part3.exercises}/>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  console.log(props)
+
+  return(
+    <div>
+      <p>
+        Course header: {props.titulo}
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+
+    return (
+      <>
+        <Part desc={props.p1} texs={props.e1} />
+        <Part desc={props.p2} texs={props.e2} />
+        <Part desc={props.p3} texs={props.e3} />
+      </>
+    )
+}
+
+const Total = (props) => {
+  console.log(props)
+
+    return(
+      <>
+        <p>Total exercises: {props.etotal}</p>
+      </>
+    )
+}
+
+const Part = (props) => {
+  console.log(props)
+
+  return (
+    <>
+      <li>
+        {props.desc}, total exercises: {props.texs}
+      </li>
+    </>
+  )
+}
+
+//Exercise 1.4
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header titulo={course}/>
+      <Content partes={parts}/>
+      <Total partes={parts}/>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  console.log(props)
+
+  return(
+    <div>
+      <p>
+        Course header: {props.titulo}
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+
+    return (
+      <>
+        <Part desc={props.partes[0].name} texs={props.partes[0].exercises} />
+        <Part desc={props.partes[1].name} texs={props.partes[1].exercises} />
+        <Part desc={props.partes[2].name} texs={props.partes[2].exercises} />
+      </>
+    )
+}
+
+const Total = (props) => {
+  console.log(props)
+
+    return(
+      <>
+        <p>Total exercises: {props.partes[0].exercises + props.partes[1].exercises
+        + props.partes[2].exercises}</p>
+      </>
+    )
+}
+
+const Part = (props) => {
+  console.log(props)
+
+  return (
+    <>
+      <li>
+        {props.desc}, total exercises: {props.texs}
+      </li>
+    </>
+  )
+}
+
+*///Exercise 1.5
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header titulo={course.name}/>
+      <Content partes={course.parts}/>
+      <Total partes={course.parts}/>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  console.log(props)
+
+  return(
+    <div>
+      <p>
+        Course header: {props.titulo}
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+
+    return (
+      <>
+        <Part desc={props.partes[0].name} texs={props.partes[0].exercises} />
+        <Part desc={props.partes[1].name} texs={props.partes[1].exercises} />
+        <Part desc={props.partes[2].name} texs={props.partes[2].exercises} />
+      </>
+    )
+}
+
+const Total = (props) => {
+  console.log(props)
+
+    return(
+      <>
+        <p>Total exercises: {props.partes[0].exercises + props.partes[1].exercises
+        + props.partes[2].exercises}</p>
+      </>
+    )
+}
+
+const Part = (props) => {
+  console.log(props)
+
+  return (
+    <>
+      <li>
+        {props.desc}, total exercises: {props.texs}
+      </li>
+    </>
+  )
+}
+
+
 ReactDOM.render(<App />, document.getElementById('root'))
--- FullstackExs/part1/src/index.js
+++ FullstackExs/part1/src/index.js
@@ -1,10 +1,35 @@
+/*
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
 const Hello = (props) => {
+
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+
   return (
     <div>
-      <p>Hello world from {props.nombre} I'm {props.edad} years old</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const Hello = ({ name, edad }) => {
+  const bornYear = () => new Date().getFullYear() - edad
+
+  return (
+    <div>
+      <li>
+        Hello {name}, you are {edad} years old
+      </li>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
@@ -21,12 +46,12 @@ const App = () => {
 
     return( //iniciando div pero con el elemento vacío
       <>
-        <p>Hola, hoy es {now.toString()}</p> 
+        <p>Hola, hoy es {now.toString()}</p>
         <p>
           {a} + {b} = {a + b}
         </p>
-        <Hello name="Carlos"/>
-        <Hello name="Sandy"/>
+        <Hello name="Carlos" edad={23} />
+        <Hello name="Sandy" edad={40-15}/>
         <Hello nombre={name} edad={age}/>
         <Footer/>
       </>//cerrando lo mismo
@@ -38,6 +63,61 @@ const Footer = () => {
     <div>
       App created by <a href="https://github.com/thomync">thomync</a>
     </div>
+  )
+}
+
+*/
+
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+/*
+const App = () => {
+
+  const [ counter, setCounter ] = useState(0)
+
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log('renderizando...', counter)
+
+  return (
+    <div>{counter}</div>
+  )
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
+*/
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+
+      <Display counter={counter}/>
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+      <button onClick={setToZero}> 
+        zero
+      </button>
+    </div>
+  )
+}
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
   )
 }
 
